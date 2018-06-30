b08b7252ec26e0b3e9b0857bbf4bb6e032dac8e9
techneplus <jamesugbanu@gmail.com>
2018-06-30 16:15:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
 //Service worker registration
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js', {scope: './'}
+    .register('/sw.js', {scope: './'})
     .then((registration) => {
         var serviceWorker;
         if (registration.installing) {
